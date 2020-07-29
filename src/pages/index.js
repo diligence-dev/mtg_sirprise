a7a1b64866d0fc7938f0761dc81eab99959bf274
diligence-dev <diligence.dev@web.de>
2020-07-29 16:56:56 +0200

    set from dropdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react'
 
-const QueryResult = ({query, expansion}) => {
+const wubrg = 'wubrg'.split('')
+const callSetter = setter => event => setter(event.target.value)
+
+const QueryResult = ({colors, expansion}) => {
   const [cardImages, setCardImages] = useState([])
 
   useEffect(() => {
-    if (query === '' || expansion.length !== 3) {
+    if (!expansion) {
+      setCardImages([])
       return
     }
 
-    const cmc = parseInt(query, 10)
-    const colors = query.slice(cmc.toString().length).trim()
-    const colorQuery = colors === '' ? '' : '+(' + colors.split('').map(c => 'c:' + c).join(' or ') + ')'
+    const colorQuery = (colors.split('').filter(c => wubrg.includes(c)) || wubrg)
+      .map(c => 'c:' + c)
+      .join(' or ')
 
     const endpoint = 'https://api.scryfall.com/cards/search?order=cmc&q='
-    const request = `${endpoint}s:${expansion}+(t:instant or o:flash)+cmc<=${cmc || '99'}${colorQuery}`
+    const request = `${endpoint}s:${expansion}+(t:instant or o:flash)+(${colorQuery})`
     fetch(request)
       .then(result => result.json())
       .then(json => {
@@ -21,7 +25,7 @@ const QueryResult = ({query, expansion}) => {
           setCardImages(json.data.map(card => card.image_uris.normal))
         }
       })
-  }, [query, expansion])
+  }, [colors, expansion])
   return (
     <div>
       <div style={{maxWidth: 1000}}>
@@ -33,20 +37,41 @@ const QueryResult = ({query, expansion}) => {
 }
 
 export default function Home() {
-  const [query, setQuery] = useState('2ur')
-  const [expansion, setExpansion] = useState('eld')
+  const [expansion, setExpansion] = useState('')
+  const [colors, setColors] = useState('')
+
+  const [allExpansion, setAllExpansions] = useState([])
+  useEffect(() => {
+    fetch('https://api.scryfall.com/sets')
+      .then(result => result.json())
+      .then(json => {
+        if (!json.data) {
+          return
+        }
+        setAllExpansions(json.data
+          .filter(expansion =>
+            !expansion.parent_set_code
+              && !['spellbook', 'promo', 'funny', 'box', 'duel_deck', 'commander'].includes(expansion.set_type)
+              && !expansion.foil_only)
+          .sort(expansion => expansion.released_at))
+      })
+  }, [])
 
   return (
     <div>
       expansion: 
-      <input type='text' value={expansion} placeholder='eld'
-        onChange={event => setExpansion(event.target.value)} />
+      <select value={expansion} onChange={callSetter(setExpansion)} onBlur={callSetter(setExpansion)}>
+        {allExpansion.map(expansion => (
+          <option value={expansion.code} key={expansion.id}>
+            {expansion.code.toUpperCase()} - {expansion.name}
+          </option>
+        ))}
+      </select>
       <br />
-      cmc and colors: 
-      <input type='text' value={query} placeholder='2ur'
-        onChange={event => setQuery(event.target.value)} />
+      colors:
+      <input type='text' value={colors} placeholder='ur' onChange={callSetter(setColors)} />
       <br />
-      <QueryResult query={query} expansion={expansion} />
+      <QueryResult colors={colors} expansion={expansion} />
     </div>
   )
 }
