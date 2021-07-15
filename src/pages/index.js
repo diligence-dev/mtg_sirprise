6cd190bb816b3803ce2804c255abb2c68ad35f51
diligence-dev <diligence.dev@web.de>
2021-07-15 11:39:21 +0200

    show castable cards only

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,68 @@
 import React, { useState, useEffect } from 'react'
-import { useLocation } from '@reach/router'
+import jsgraphs from 'js-graph-algorithms'
 
 const wubrg = 'wubrg'.split('')
 const callSetter = setter => event => setter(event.target.value)
 
-const QueryResult = ({colors, expansion}) => {
+const isCastable = (openMana, manaCost) => {
+  openMana = openMana.toUpperCase().match(/[WUBRGC]/g)
+
+  const graph = new jsgraphs.FlowNetwork(14)
+  const source = 0
+  const sink = 13
+  const openManaNode = {
+    W: 1,
+    U: 2,
+    B: 3,
+    R: 4,
+    G: 5,
+    C: 6
+  }
+  const manaCostNode = {
+    W: 7,
+    U: 8,
+    B: 9,
+    R: 10,
+    G: 11,
+    C: 12
+  }
+
+  openMana.forEach(x => graph.addEdge(new jsgraphs.FlowEdge(source, openManaNode[x], 1)))
+
+  let flowNeeded = 0
+  manaCost
+    .toUpperCase()
+    .replace('{X}', '')
+    .match(/\{([0-9]+|[WUBRG])\}/g)
+    .map(x => x.replace('{', '').replace('}', ''))
+    .forEach(x => {
+      if (wubrg.includes(x.toLowerCase())) {
+        graph.addEdge(new jsgraphs.FlowEdge(manaCostNode[x], sink, 1))
+        flowNeeded += 1
+      } else {
+        graph.addEdge(new jsgraphs.FlowEdge(manaCostNode['C'], sink, parseInt(x, 10)))
+        flowNeeded += parseInt(x, 10)
+      }
+    })
+
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['W'], manaCostNode['W'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['U'], manaCostNode['U'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['B'], manaCostNode['B'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['R'], manaCostNode['R'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['G'], manaCostNode['G'], 1000))
+
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['W'], manaCostNode['C'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['U'], manaCostNode['C'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['B'], manaCostNode['C'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['R'], manaCostNode['C'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['G'], manaCostNode['C'], 1000))
+  graph.addEdge(new jsgraphs.FlowEdge(openManaNode['C'], manaCostNode['C'], 1000))
+
+  const maxFlow = new jsgraphs.FordFulkerson(graph, source, sink).value
+  return maxFlow === flowNeeded
+}
+
+const QueryResult = ({openMana, expansion}) => {
   const [cardImages, setCardImages] = useState([])
 
   useEffect(() => {
@@ -13,7 +71,14 @@ const QueryResult = ({colors, expansion}) => {
       return
     }
 
-    const colorQuery = (colors.split('').filter(c => wubrg.includes(c)) || wubrg)
+    if (openMana === '') {
+      openMana = 'wubrg'
+    }
+
+    const colorQuery = openMana
+      .split('')
+      .filter(c => wubrg.includes(c.toLowerCase()))
+      .concat(['colorless'])
       .map(c => 'c:' + c)
       .join(' or ')
 
@@ -23,10 +88,18 @@ const QueryResult = ({colors, expansion}) => {
       .then(result => result.json())
       .then(json => {
         if (json.data) {
-          setCardImages(json.data.map(card => (card.image_uris || card.card_faces[0].image_uris).normal))
+          setCardImages(json.data
+            .filter(card => {
+              if (openMana.length === 0) {
+                return true
+              }
+              return isCastable(openMana, card.mana_cost || card.card_faces[0].mana_cost)
+            })
+            .map(card => (card.image_uris || card.card_faces[0].image_uris).normal)
+          )
         }
       })
-  }, [colors, expansion])
+  }, [openMana, expansion])
   return (
     <div>
       <div style={{maxWidth: 1000}}>
@@ -37,9 +110,9 @@ const QueryResult = ({colors, expansion}) => {
   )
 }
 
-export default function Home() {
-  const [expansion, setExpansion] = useState(useLocation().search.slice(1))
-  const [colors, setColors] = useState('')
+export default function Home({location}) {
+  const [expansion, setExpansion] = useState(location.search.replace('?', ''))
+  const [openMana, setOpenMana] = useState('')
 
   const [allExpansion, setAllExpansions] = useState([])
   useEffect(() => {
@@ -69,10 +142,10 @@ export default function Home() {
         ))}
       </select>
       <br />
-      colors:
-      <input type='text' value={colors} placeholder='ur' onChange={callSetter(setColors)} />
+      openMana:
+      <input type='text' value={openMana} placeholder='ur' onChange={callSetter(setOpenMana)} />
       <br />
-      <QueryResult colors={colors} expansion={expansion} />
+      <QueryResult openMana={openMana} expansion={expansion} />
     </div>
   )
 }
