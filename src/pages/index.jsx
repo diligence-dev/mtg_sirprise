fecd2f287dcf3da116f193f2122489ab1a20bbbd
diligence-dev <diligence.dev@web.de>
2021-07-22 12:22:28 +0200

    style + tooltip

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import ReactTooltip from 'react-tooltip'
 
 import { isCastable } from '../mtg_sirprise_lib.mjs'
 
@@ -31,11 +32,10 @@ const QueryResult = ({openMana, expansion}) => {
       })
   }, [openMana, expansion])
   return (
-    <div>
-      <div style={{maxWidth: 1000}}>
-        {cardImages.map(imageUrl => <img width='200' src={imageUrl} alt='' key={imageUrl} />)}
-      </div>
-      <br />
+    <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', marginTop: '8px'}}>
+      {cardImages.map(imageUrl =>
+        <img style={{width: '200px', padding: '1px'}} src={imageUrl} alt='' key={imageUrl} />
+      )}
     </div>
   )
 }
@@ -65,21 +65,36 @@ export default function Home({location}) {
       })
   }, [expansion])
 
+  const formFieldStyle = {margin: '2px', width: '300px'}
+  const openManaHelp = ('Use the color symbols "wubrgc" to define what open mana your opponent has.<br />' +
+    'Use brackets if a land can produce one of multiple colors.<br />' +
+    'For example if your opponent has two Islands, a Treasure Map and a Raugrin Triome: "uuc[urw]"')
   return (
-    <div>
-      expansion: 
-      <select value={expansion} onChange={callSetter(setExpansion)} onBlur={callSetter(setExpansion)}>
-        {allExpansions.map(expansion => (
-          <option value={expansion.code} key={expansion.id}>
-            {expansion.code.toUpperCase()} - {expansion.name}
-          </option>
-        ))}
-      </select>
-      <br />
-      openMana:
-      <input type='text' value={openMana} placeholder='u[rw]' onChange={callSetter(setOpenMana)} />
-      <br />
+    <div style={{minWidth: '616px', display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <h1 style={{fontSize: '30px'}}>MtG Sirprise</h1>
+      <div>
+        <span>
+          expansion:
+        </span>
+        <select
+            value={expansion} style={formFieldStyle} onChange={callSetter(setExpansion)}
+            onBlur={callSetter(setExpansion)}>
+          {allExpansions.map(expansion => (
+            <option value={expansion.code} key={expansion.id}>
+              {expansion.code.toUpperCase()} - {expansion.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <span>
+          open mana:
+        </span>
+        <input type='text' value={openMana}  style={formFieldStyle} placeholder='u[rw]'
+            onChange={callSetter(setOpenMana)} data-tip={openManaHelp} data-multiline={true} />
+      </div>
       <QueryResult openMana={openMana} expansion={expansion} />
+      <ReactTooltip />
     </div>
   )
 }
